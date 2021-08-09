4290b4717fd44b0611a9ae8e988ee366d72e4299
DHV10 <supervietfs@gmail.com>
2021-08-09 14:56:26 +0700

    Initial Commit

---
+++ Netflix-Clone/Netflix_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netflix_CloneApp.swift
+//  Netflix-Clone
+//
+//  Created by DHV on 09/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct Netflix_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
